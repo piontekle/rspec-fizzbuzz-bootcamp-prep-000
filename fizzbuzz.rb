85e5b86e246a5681cbf9361931111944f6dcae95
Lauren Piontek <piontekle@gmail.com>
2018-10-22 17:53:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    return "Fizz"
     elsif int % 5 == 0
-    puts "Buzz"
+    return "Buzz"
     elsif int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    return "FizzBuzz"
 end
 end
